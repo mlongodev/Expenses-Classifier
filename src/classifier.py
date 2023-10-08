dec4bc6a1e5d8353f0eca89b2bd1ea6f4433a5ff
Mario Longo <mariolongo89@gmail.com>
2023-10-09 01:15:35 +0200

    added try catch

--- src/classifier.py
+++ src/classifier.py
@@ -4,30 +4,35 @@ import os
 
 
 def get_label(desc):
-    load_dotenv();
-    if 'PROMPT_CAT' in os.environ:
-        prompt_cat_list = os.environ['PROMPT_CAT']
-    else:
-        # Default categories
-        prompt_cat_list = "House rent, Supermarket, Internet home, Mobile phone, Gas, Electricity, Bank charges (card, taxes), Online services, Restaurants, Delivery, Aperitifs/bars, Shopping for Home, Clothes, Health, Courses, Technology, Transportation (plane, bus, subway, car)."
+    try:
+        load_dotenv();
+        if 'PROMPT_CAT' in os.environ:
+            prompt_cat_list = os.environ['PROMPT_CAT']
+        else:
+            # Default categories
+            prompt_cat_list = "House rent, Supermarket, Internet home, Mobile phone, Gas, Electricity, Bank charges (card, taxes), Online services, Restaurants, Delivery, Aperitifs/bars, Shopping for Home, Clothes, Health, Courses, Technology, Transportation (plane, bus, subway, car)."
 
-    if 'PROMPT_PRIVATE_INFO' in os.environ:
-        prompt_private_info = os.environ['PROMPT_PRIVATE_INFO']
-    else:
-        prompt_private_info = ''
+        if 'PROMPT_PRIVATE_INFO' in os.environ:
+            prompt_private_info = os.environ['PROMPT_PRIVATE_INFO']
+        else:
+            prompt_private_info = ''
 
-    content_system = ("I would like to classify my expenses using specific categories. In input you will have the list of categories and the description of an expense. Please associate a category to the expense. "
-                      "Please only respond with the exact name of the category listed and nothing else. " 
-                     + "Categories: \n") + prompt_cat_list + "\n" + prompt_private_info
+        content_system = ("I would like to classify my expenses using specific categories. In input you will have the list of categories and the description of an expense. Please associate a category to the expense. "
+                        "Please only respond with the exact name of the category listed and nothing else. " 
+                        + "Categories: \n") + prompt_cat_list + "\n" + prompt_private_info
 
-    openai.organization = os.environ['OPENAI_ORG_ID']
-    openai.api_key = os.environ['OPENAI_API_KEY']
-    completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": content_system},
-        {"role": "user", "content": "Expense description: " + str(desc)}]
-    )
-    data = completion.choices[0].message
+        openai.organization = os.environ['OPENAI_ORG_ID']
+        openai.api_key = os.environ['OPENAI_API_KEY']
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": content_system},
+            {"role": "user", "content": "Expense description: " + str(desc)}]
+        )
+        data = completion.choices[0].message
 
-    return data["content"]
+        return data["content"]
+    except Exception as e:
+        # Handle the exception (e.g., log the error)
+        print(f"An error occurred: {e}")
+        return " "
