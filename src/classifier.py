c0797b1bb2b7d098461681da66a8c5543af912d6
Mario Longo <mariolongo89@gmail.com>
2024-06-30 21:57:13 +0200

    chunk request test

--- src/classifier.py
+++ src/classifier.py
@@ -1,4 +1,5 @@
 import openai
+import json
 from dotenv.main import load_dotenv
 import os
 
@@ -30,6 +31,33 @@ def get_label(desc):
             {"role": "user", "content": "Expense description: " + str(desc)}]
         )
         data = completion.choices[0].message
+
+        return data["content"]
+    except Exception as e:
+        # Handle the exception (e.g., log the error)
+        print(f"An error occurred: {e}")
+        return " "
+    
+def get_categories(chunk_data):
+    try:
+        load_dotenv();
+        content_system = ('''You are an assistant who has the task of classifying my expenses using specific categories. In input you will have the list of categories and the description of an expense. Please associate a category to the expense. Please only respond with the exact name of the category listed and nothing else.\n 
+                          Categories: House rent, Supermarket, Internet home, Mobile phone, Spotify, Netflix, Gas, Electricity, Bank charges (card, taxes), Other online services, Other (Fixed), Restaurants, Delivery, Aperitifs/bars, Shopping for Home, Clothes, Health, Courses, Technology, Transportation.
+                          Other info: SI GUSTA is always Supermarket, dm drogerie is always Supermarket, laESSE is always Supermarket, CLESS TICKET ATM MILAN is always Transportation, Aspit Direz is always Transportation, SPOTIFY is always the Spotify category, Storytel is always Other online services category, Fastweb Spa is always Internet home, Martina Bregola in related with the House Rent category, Glovo is always Delivery, “A2a Spa Codice Mandato S9278668598” is always Electricity, “A2a Spa Codice Mandato S9231568484” is always Gas, Prime Video is always Other online services; Saf Srl is always Other.\n
+                          Give me the response on a array Json. It should have the fields "id" with the id of the expense and "category" with the category for each expenses.''')
+
+        openai.organization = os.environ['OPENAI_ORG_ID']
+        openai.api_key = os.environ['OPENAI_API_KEY']
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-1106",
+        messages=[
+            {"role": "system", "content": content_system},
+            {"role": "user", "content": "Expenses description: " + str(chunk_data)}],
+        response_format={ "type": "json_object" }
+        )
+        data = completion.choices[0].message
+        print("NEW CALL")
+        print(data)
 
         return data["content"]
     except Exception as e:
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import openai
 import time
 from data_handling import get_desc_column, get_creditcard_entry, get_bank_acc_entry, get_revolut_entry, get_label_column 
-from classifier import get_label
+from classifier import get_label, get_categories
 
 
 dfs = [] #list of dataframes
@@ -19,11 +19,24 @@ conc_df = pd.concat(dfs,
 conc_df[get_label_column()] = None
 conc_df.reset_index(drop=True, inplace=True)
 
-for index, row in conc_df.iterrows(): 
-    #label = str(get_label(row[get_desc_column()])) #Uncomment this line to enable GPT
-    label = str('') #Comment this line to enable GPT
-    conc_df.at[index, get_label_column()] = label
-    print(str(row[get_desc_column()]) + ' - ' + label)
-    time.sleep(0.5)
+chunk_size = 20
+# Creazione della lista di dataframe divisi in chunk
+list_df = [conc_df[i:i+chunk_size] for i in range(0, conc_df.shape[0], chunk_size)]
+
+for i, data_chunk in enumerate(list_df):
+    csv_text= data_chunk.to_csv(index=True)
+    print(csv_text)
+    get_categories(csv_text)
+    time.sleep(5)
+
+
+#print(csv_text)
+
+#for index, row in conc_df.iterrows(): 
+#    #label = str(get_label(row[get_desc_column()])) #Uncomment this line to enable GPT
+#    label = str('') #Comment this line to enable GPT
+#    conc_df.at[index, get_label_column()] = label
+#    print(str(row[get_desc_column()]) + ' - ' + label)
+#    time.sleep(0.5)
 
 conc_df.to_excel('files/output.xlsx', index=False)
